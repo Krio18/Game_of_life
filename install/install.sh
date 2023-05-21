f696914a414ea74590802bc437dde5d18aede104
Krio18 <killian.cottrelle@epitech.eu>
2023-05-21 23:40:52 +0200

    [ADD] base of game of life release

---
+++ install/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RGREEN='\033[0m'
+LGREEN='\033[4;32m'
+
+LIGHT_GREEN='\033[1;32m'
+
+# Compilation du programme
+make -s
+
+
+# Déplacement des binaire vers un emplacement approprié
+sudo mv src/*.o ./Bin
+sudo mv lib/my/*.o ./Bin
+
+# Permissions d'exécution pour l'utilisateur
+sudo cp install/cell /usr/local/bin/cell
+sudo chmod +x /usr/local/bin/cell
+
+echo ""
+echo -e "${LGREEN}'Game of Life' Success.${RGREEN} ${LIGHT_GREEN}You can run 'Game of life' with 'cell' commande."
+echo ""
